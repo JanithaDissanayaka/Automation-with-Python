66c18d281e710e096ba5de8d76a07f5bbeb67290
JanithaDissanayaka <janithadissnayakaa@gmail.com>
2026-01-08 11:15:03 +0530

    take aws details

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import boto3
 
 ec2_client = boto3.client('ec2')
-all_avilable_vpc=ec2_client.describe_vpcs()
-print(all_avilable_vpc)
+all_avilable_vpcs=ec2_client.describe_vpcs()
+vpcs=all_avilable_vpcs["Vpcs"]
+
+for vpc in vpcs:
+    print(vpc["VpcId"])
+    association_set=vpc["CidrBlockAssociationSet"]
+    for set in association_set:
+        print(set["CidrBlock"])
+        print(set["CidrBlockState"])

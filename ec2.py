25cbd242644cadbc98df7e5b78654977e35935e1
JanithaDissanayaka <janithadissnayakaa@gmail.com>
2026-01-09 19:11:44 +0530

    change to shedule

--- ec2.py
+++ ec2.py
@@ -1,20 +1,22 @@
 import boto3
+import schedule
+
+
 ec2_client = boto3.client('ec2')
 ec2_resource=boto3.resource('ec2')
 
-reservations=ec2_client.describe_instances()
+def instance_statuses():
+    statuses=ec2_client.describe_instance_status()
+    for status in statuses['InstanceStatuses']:
+        int_status=status['InstanceStatus']['Status']
+        sys_status=status['SystemStatus']['Status']
+        state = status['InstanceState']['Name']
 
 
-for reservation in reservations['Reservations']:
-    instances=reservation['Instances']
-    for instance in instances:
-        print(f"Instance {instance['InstanceId']}is {instance['State']}")
+        print(f"Instance {status['InstanceId']} is {state} with instance status {int_status} and System status is {sys_status}")
 
+schedule.every(10).seconds.do(instance_statuses)
 
-statuses=ec2_client.describe_instance_status()
-
-for status in statuses['InstanceStatuses']:
-    int_status=status['InstanceStatus']['Status']
-    sys_status=status['SystemStatus']['Status']
-
-    print(f"Instance {status['InstanceId']} status is {int_status} and System status is {sys_status}")
+while True:
+    schedule.run_pending()
+    

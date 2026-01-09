514704ff40a6a8b7bc6aecbfa93d5b46fe70c241
JanithaDissanayaka <janithadissnayakaa@gmail.com>
2026-01-09 20:37:31 +0530

    add more steps

--- ec2.py
+++ ec2.py
@@ -6,17 +6,19 @@ ec2_client = boto3.client('ec2')
 ec2_resource=boto3.resource('ec2')
 
 def instance_statuses():
-    statuses=ec2_client.describe_instance_status()
+    statuses=ec2_client.describe_instance_status(IncludeAllInstances=True)
     for status in statuses['InstanceStatuses']:
         int_status=status['InstanceStatus']['Status']
         sys_status=status['SystemStatus']['Status']
         state = status['InstanceState']['Name']
 
 
+
         print(f"Instance {status['InstanceId']} is {state} with instance status {int_status} and System status is {sys_status}")
+        print("#####################################\n")
 
 schedule.every(10).seconds.do(instance_statuses)
 
 while True:
     schedule.run_pending()
-    
+
